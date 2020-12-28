d3fc8ad70b496dfda88738ac360cba86f7f7d47a
Kangbeom Kim <kkb931115@gmail.com>
2020-12-29 01:47:56 +0900

    TopBar 컴포넌트 변경 Nextjs, React, Nodejs 이미지 삽입

--- components/TopBar.jsx
+++ components/TopBar.jsx
@@ -1,10 +1,14 @@
 import style from '../styles/style.module.css';
 import { FaBars } from 'react-icons/fa';
+import Image from 'next/image';
 
 function TopBar() {
   return (
     <header className={style.headerBar}>
       <FaBars />
+      <Image src="/nextjs-logo.svg" width={90} height={50} />
+      <Image src="/react-logo.svg" width={60} height={50} />
+      <Image src="/nodejs-logo.svg" width={100} height={50} />
       This is top bar
     </header>
   );
