d5d78eeea178b0728851d6024596103ef32bdb04
Kangbeom Kim <kkb931115@gmail.com>
2020-12-29 02:02:34 +0900

    TopBar 리팩토링
    * 이미지에 대한 정보를 가지는 배열의 엘리먼트를 객체로 할당, map 으로 반복
    * 관련 css 수정

--- components/TopBar.jsx
+++ components/TopBar.jsx
@@ -2,14 +2,48 @@ import style from '../styles/style.module.css';
 import { FaBars } from 'react-icons/fa';
 import Image from 'next/image';
 
+const imgs = [
+  {
+    src: '/nextjs-logo.svg',
+    width: 90,
+    height: 50,
+    alt: 'Next.js',
+    title: '리액트 서버 사이드 렌더링 프레임워크 입니다.',
+  },
+  {
+    src: '/react-logo.svg',
+    width: 60,
+    height: 50,
+    alt: 'React.js',
+    title: 'SPA 제작 프론트엔드 라이브러리 입니다.',
+  },
+  {
+    src: '/nodejs-logo.svg',
+    width: 100,
+    height: 50,
+    alt: 'Node.js',
+    title: '크로미움 V8 자바스크립트 런타임 입니다.',
+  },
+];
+
+function Logo({ src, width, height, alt, title }) {
+  return (
+    <Image src={src} width={width} height={height} alt={alt} title={title} />
+  );
+}
 function TopBar() {
   return (
     <header className={style.headerBar}>
       <FaBars />
-      <Image src="/nextjs-logo.svg" width={90} height={50} />
-      <Image src="/react-logo.svg" width={60} height={50} />
-      <Image src="/nodejs-logo.svg" width={100} height={50} />
-      This is top bar
+      {imgs.map((logo) => (
+        <Logo
+          src={logo.src}
+          width={logo.width}
+          height={logo.height}
+          alt={logo.alt}
+          title={logo.title}
+        />
+      ))}
     </header>
   );
 }
