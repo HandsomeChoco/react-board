43304a38c811323809a1bd75c5eb57fa8fd700b0
HandsomeChoco <kkb931115@gmail.com>
2020-12-29 01:28:34 +0900

    공통 레이아웃 TopBar 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from '../components/Layout';
 import SideBar from '../components/SideBar';
+import TopBar from '../components/TopBar';
 import '../styles/globals.css';
 
 function App({ Component, pageProps }) {
   return (
     <Layout>
+      <TopBar />
       <SideBar />
       <Component {...pageProps} />
     </Layout>
