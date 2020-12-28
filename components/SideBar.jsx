40af8d191da6343c8da0739f3bf65873ff700101
HandsomeChoco <kkb931115@gmail.com>
2020-12-29 01:25:43 +0900

    state 관리 및 보이기/숨기기 이벤트 핸들러 작성 및 태그 변경

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,7 +1,34 @@
 import style from '../styles/style.module.css';
+import { IoMdClose } from 'react-icons/io';
+import { useState } from 'react';
+
+const hide = {
+  width: '0px',
+  padding: '0px',
+};
+
+const show = {
+  width: '320px',
+  padding: '8px',
+};
 
 function SideBar() {
-  return <section className={style.sideBar}> this is side bar</section>;
+  const [show, setShow] = useState({ visible: false });
+  const { visible } = show;
+
+  const showSideBar = () => setShow({ visible: !visible });
+
+  return (
+    <section className={style.sideBar} style={visible ? show : hide}>
+      <div className={style.closeSideBarIcon}>
+        <IoMdClose onClick={showSideBar} />
+      </div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </section>
+  );
 }
 
 export default SideBar;
