24927b6fc3b1eed5cf1bf572b1adb8038f243eb0
김강범 <kangbeomkim@gimgangbeom-ui-MacBookPro.local>
2020-12-31 00:31:33 +0900

    SideBar 기 GlobalContext 와 인터렉션 할 수 있는 코드 작성
    X 버튼을 누르면 사이드바가 닫힙니다.

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,6 +1,6 @@
 import style from '../styles/style.module.css';
 import { IoMdClose } from 'react-icons/io';
-import { useState } from 'react';
+import { useGlobalState, useGlobalDispatch } from '../reducer/GlobalContext';
 
 const hide = {
   width: '0px',
@@ -13,17 +13,23 @@ const show = {
 };
 
 function SideBar() {
-  const [show, setShow] = useState({ visible: false });
-  const { visible } = show;
+  const state = useGlobalState();
+  const dispatch = useGlobalDispatch();
+  const { ui } = state;
 
-  const showSideBar = () => setShow({ visible: !visible });
+  const { visible } = ui.sideBar;
 
   return (
     <section className={style.sideBar} style={visible ? show : hide}>
       <div className={style.closeSideBarIcon}>
-        <IoMdClose onClick={showSideBar} />
+        <IoMdClose
+          onClick={() => {
+            dispatch({ type: 'ON_TOGGLE_SIDEBAR' });
+          }}
+        />
       </div>
-      <div>1</div>
+
+      <div>{visible}</div>
       <div>2</div>
       <div>3</div>
       <div>4</div>
