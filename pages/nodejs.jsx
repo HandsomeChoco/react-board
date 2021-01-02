7d797b63253d403c2373c722f58d2f426ff7a960
김강범 <kangbeomkim@gimgangbeom-ui-MacBookPro.local>
2021-01-03 03:51:31 +0900

    게시판 라우팅 할 페이지 3개 추가

---
+++ pages/nodejs.jsx
@@ -0,0 +1,14 @@
+import Container from '../components/Container';
+
+import TextTitle from '../components/TextTitle';
+import Block from '../components/Block';
+
+function Nodejs() {
+  return (
+    <Block>
+      <TextTitle tag="h2" text="Nodejs 게시판이에요" />
+    </Block>
+  );
+}
+
+export default Nodejs;
---
+++ pages/react.jsx
@@ -0,0 +1,12 @@
+import Block from '../components/Block';
+import TextTitle from '../components/TextTitle';
+
+function React() {
+  return (
+    <Block>
+      <TextTitle tag="h2" text="React 게시판이에요" />
+    </Block>
+  );
+}
+
+export default React;
