ad3f73116f799459bcda74a929d48e1a85ff882d
김강범 <kangbeomkim@gimgangbeom-ui-MacBookPro.local>
2021-01-03 03:56:00 +0900

    BoardItemWrapper => BoardItem 으로 컴포넌트 이름 변경

---
+++ components/BoardItem.jsx
@@ -0,0 +1,17 @@
+import style from '../styles/style.module.css';
+
+function Item({ text }) {
+  return <li>{text}</li>;
+}
+
+function BoardItem({ arr }) {
+  return (
+    <div className={style.boardItem}>
+      {arr.map((v, i) => (
+        <Item key={i} text={v.text} />
+      ))}
+    </div>
+  );
+}
+
+export default BoardItem;
