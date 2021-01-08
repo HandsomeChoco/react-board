996747266fd62a2c471c6973cda6794bae4ede30
Kangbeom Kim <kkb931115@gmail.com>
2021-01-09 02:27:08 +0900

    Item 컴포넌트가 src props 도 받아올 수 있도록 변경

--- src/components/BoardItem.jsx
+++ src/components/BoardItem.jsx
@@ -1,14 +1,21 @@
+import Link from 'next/link';
 import style from '../../styles/style.module.css';
 
-function Item({ text }) {
-  return <li>{text}</li>;
+function Item({ text, src }) {
+  return (
+    <li>
+      <Link href={src}>
+        <a style={{ textDecoration: 'none' }}>{text}</a>
+      </Link>
+    </li>
+  );
 }
 
 function BoardItem({ arr }) {
   return (
     <div className={style.boardItem}>
       {arr.map((v, i) => (
-        <Item key={i} text={v.text} />
+        <Item key={i} text={v.text} src={v.src} />
       ))}
     </div>
   );
